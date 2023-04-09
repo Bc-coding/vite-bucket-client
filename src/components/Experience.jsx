815f5fe521fa7ecacbb5d0889343f02234bb7219
Belle Carrie <bc.coding@hotmail.com>
2023-04-09 19:53:35 +0100

    added other matcap purple and shine

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -11,7 +11,10 @@ import { Perf } from "r3f-perf";
 
 const boxGeometry = new THREE.BoxGeometry();
 const sphereGeometry = new THREE.SphereGeometry();
-const material = new THREE.MeshMatcapMaterial();
+
+const material_blue = new THREE.MeshMatcapMaterial();
+const material_purple = new THREE.MeshMatcapMaterial();
+const material_shine = new THREE.MeshMatcapMaterial();
 
 export default function Experience() {
   const boxGroup = useRef();
@@ -22,16 +25,41 @@ export default function Experience() {
     }
   });
 
-  const [matcapTexture] = useMatcapTexture("1D3FCC_051B5F_81A0F2_5579E9", 256);
+  const [matcapTexture_blue] = useMatcapTexture(
+    "1D3FCC_051B5F_81A0F2_5579E9",
+    256
+  );
+
+  const [matcapTexture_purple] = useMatcapTexture(
+    "5B4CBC_B59AF2_9B84EB_8F78E4",
+    256
+  );
+
+  const [matcapTexture_shine] = useMatcapTexture(
+    "8955D0_744CC4_EA4AEF_954DA4",
+    256
+  );
 
   //console.log(matcapTexture);
 
   useEffect(() => {
-    matcapTexture.encoding = THREE.sRGBEncoding;
-    matcapTexture.needsUpdate = true;
+    matcapTexture_blue.encoding = THREE.sRGBEncoding;
+    matcapTexture_blue.needsUpdate = true;
 
-    material.matcap = matcapTexture;
-    material.needsUpdate = true;
+    material_blue.matcap = matcapTexture_blue;
+    material_blue.needsUpdate = true;
+
+    matcapTexture_purple.encoding = THREE.sRGBEncoding;
+    matcapTexture_purple.needsUpdate = true;
+
+    material_purple.matcap = matcapTexture_purple;
+    material_purple.needsUpdate = true;
+
+    matcapTexture_shine.encoding = THREE.sRGBEncoding;
+    matcapTexture_shine.needsUpdate = true;
+
+    material_shine.matcap = matcapTexture_shine;
+    material_shine.needsUpdate = true;
   }, []);
 
   return (
@@ -42,7 +70,7 @@ export default function Experience() {
 
       <Center>
         <Text3D
-          material={material}
+          material={material_blue}
           font="./fonts/helvetiker_regular.typeface.json"
           size={0.75}
           height={1.2}
@@ -60,7 +88,7 @@ export default function Experience() {
           <mesh
             key={i}
             geometry={boxGeometry}
-            material={material}
+            material={material_shine}
             position={[
               (Math.random() - 0.5) * 10,
               (Math.random() - 0.5) * 10,
@@ -80,7 +108,7 @@ export default function Experience() {
           <mesh
             key={i}
             geometry={sphereGeometry}
-            material={material}
+            material={material_purple}
             position={[
               (Math.random() - 0.5) * 10,
               (Math.random() - 0.5) * 10,
